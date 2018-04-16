79e9f682034eee8c5b46a2a5ba999cebcb9db55e
xiaoshi WEN <love2fuji@outlook.com>
2018-04-16 23:26:50 +0800

    可以实现TCP通讯，自动回复

---
+++ IoTServer/Main.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace IoTServer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnImportDepart = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.dgvShowDepart = new System.Windows.Forms.DataGridView();
+            this.tpgDepart = new System.Windows.Forms.TabPage();
+            this.dgvShowRegion = new System.Windows.Forms.DataGridView();
+            this.tpgRegion = new System.Windows.Forms.TabPage();
+            this.ServerLog = new System.Windows.Forms.RichTextBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabConServerLog = new System.Windows.Forms.TabControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowDepart)).BeginInit();
+            this.tpgDepart.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowRegion)).BeginInit();
+            this.tpgRegion.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabConServerLog.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnStop.Location = new System.Drawing.Point(214, 30);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(132, 62);
+            this.btnStop.TabIndex = 17;
+            this.btnStop.Text = "停止服务";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnImportDepart
+            // 
+            this.btnImportDepart.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnImportDepart.Location = new System.Drawing.Point(494, 30);
+            this.btnImportDepart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnImportDepart.Name = "btnImportDepart";
+            this.btnImportDepart.Size = new System.Drawing.Size(170, 62);
+            this.btnImportDepart.TabIndex = 16;
+            this.btnImportDepart.Text = "查看日志";
+            this.btnImportDepart.UseVisualStyleBackColor = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "短信服务";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem1,
+            this.退出ToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 60);
+            // 
+            // 显示ToolStripMenuItem1
+            // 
+            this.显示ToolStripMenuItem1.Name = "显示ToolStripMenuItem1";
+            this.显示ToolStripMenuItem1.Size = new System.Drawing.Size(116, 28);
+            this.显示ToolStripMenuItem1.Text = "显示";
+            // 
+            // 退出ToolStripMenuItem1
+            // 
+            this.退出ToolStripMenuItem1.Name = "退出ToolStripMenuItem1";
+            this.退出ToolStripMenuItem1.Size = new System.Drawing.Size(116, 28);
+            this.退出ToolStripMenuItem1.Text = "退出";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.Control;
+            this.btnStart.Location = new System.Drawing.Point(48, 30);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(135, 62);
+            this.btnStart.TabIndex = 19;
+            this.btnStart.Text = "启动服务";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // dgvShowDepart
+            // 
+            this.dgvShowDepart.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvShowDepart.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvShowDepart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShowDepart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvShowDepart.Location = new System.Drawing.Point(4, 4);
+            this.dgvShowDepart.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvShowDepart.Name = "dgvShowDepart";
+            this.dgvShowDepart.RowHeadersWidth = 10;
+            this.dgvShowDepart.RowTemplate.Height = 23;
+            this.dgvShowDepart.Size = new System.Drawing.Size(1737, 735);
+            this.dgvShowDepart.TabIndex = 0;
+            // 
+            // tpgDepart
+            // 
+            this.tpgDepart.Controls.Add(this.dgvShowDepart);
+            this.tpgDepart.Location = new System.Drawing.Point(4, 28);
+            this.tpgDepart.Margin = new System.Windows.Forms.Padding(4);
+            this.tpgDepart.Name = "tpgDepart";
+            this.tpgDepart.Padding = new System.Windows.Forms.Padding(4);
+            this.tpgDepart.Size = new System.Drawing.Size(1745, 743);
+            this.tpgDepart.TabIndex = 2;
+            this.tpgDepart.Text = " ";
+            this.tpgDepart.UseVisualStyleBackColor = true;
+            // 
+            // dgvShowRegion
+            // 
+            this.dgvShowRegion.AllowDrop = true;
+            this.dgvShowRegion.AllowUserToAddRows = false;
+            this.dgvShowRegion.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvShowRegion.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvShowRegion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShowRegion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvShowRegion.Location = new System.Drawing.Point(4, 4);
+            this.dgvShowRegion.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvShowRegion.Name = "dgvShowRegion";
+            this.dgvShowRegion.RowHeadersWidth = 50;
+            this.dgvShowRegion.RowTemplate.Height = 23;
+            this.dgvShowRegion.Size = new System.Drawing.Size(1737, 735);
+            this.dgvShowRegion.TabIndex = 0;
+            // 
+            // tpgRegion
+            // 
+            this.tpgRegion.AutoScroll = true;
+            this.tpgRegion.Controls.Add(this.dgvShowRegion);
+            this.tpgRegion.Location = new System.Drawing.Point(4, 28);
+            this.tpgRegion.Margin = new System.Windows.Forms.Padding(4);
+            this.tpgRegion.Name = "tpgRegion";
+            this.tpgRegion.Padding = new System.Windows.Forms.Padding(4);
+            this.tpgRegion.Size = new System.Drawing.Size(1745, 743);
+            this.tpgRegion.TabIndex = 1;
+            this.tpgRegion.Text = "区域/部门基础数据";
+            this.tpgRegion.UseVisualStyleBackColor = true;
+            // 
+            // ServerLog
+            // 
+            this.ServerLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ServerLog.Location = new System.Drawing.Point(4, 4);
+            this.ServerLog.Margin = new System.Windows.Forms.Padding(4);
+            this.ServerLog.Name = "ServerLog";
+            this.ServerLog.Size = new System.Drawing.Size(1348, 851);
+            this.ServerLog.TabIndex = 24;
+            this.ServerLog.Text = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.ServerLog);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(1356, 859);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "服务日志";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabConServerLog
+            // 
+            this.tabConServerLog.Controls.Add(this.tabPage1);
+            this.tabConServerLog.Controls.Add(this.tpgRegion);
+            this.tabConServerLog.Controls.Add(this.tpgDepart);
+            this.tabConServerLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabConServerLog.Location = new System.Drawing.Point(4, 25);
+            this.tabConServerLog.Margin = new System.Windows.Forms.Padding(4);
+            this.tabConServerLog.Name = "tabConServerLog";
+            this.tabConServerLog.SelectedIndex = 0;
+            this.tabConServerLog.Size = new System.Drawing.Size(1364, 891);
+            this.tabConServerLog.TabIndex = 28;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tabConServerLog);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 112);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(1372, 920);
+            this.groupBox2.TabIndex = 36;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "状态显示";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnImportDepart);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1372, 112);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选项";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1372, 1032);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowDepart)).EndInit();
+            this.tpgDepart.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShowRegion)).EndInit();
+            this.tpgRegion.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabConServerLog.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnImportDepart;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView dgvShowDepart;
+        private System.Windows.Forms.TabPage tpgDepart;
+        public System.Windows.Forms.DataGridView dgvShowRegion;
+        private System.Windows.Forms.TabPage tpgRegion;
+        private System.Windows.Forms.RichTextBox ServerLog;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabConServerLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ IoTServer/Main.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+using log4net.Config;
+
+namespace IoTServer
+{
+    public partial class Main : Form
+    {
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 显示软件运行日志
+        /// </summary>
+        /// <param name="info"></param>
+        public delegate void ShowLogEventHandler(string info);
+        public void ShowLog(string info)
+        {
+            if (this.ServerLog.InvokeRequired)
+            {
+                ShowLogEventHandler showLogHandler = new ShowLogEventHandler(this.ShowLog);
+                this.ServerLog.BeginInvoke(showLogHandler, info);
+            }
+            else
+            {
+                string s = "";
+                for (int i = 0; i < this.ServerLog.Lines.Length; i++)
+                {
+                    if (i > 200)
+                        break;
+                    s += ("\r\n" + this.ServerLog.Lines[i]);
+                }
+                this.ServerLog.Text = (DateTime.Now.ToString("yyy-MM-dd HH:mm:ss") + " >>>  " + info + s);
+            }
+        }
+
+
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartListening();
+            ShowLog("启动服务...");
+            log.Info("启动服务...");
+        }
+
+
+
+        // State object for reading client data asynchronously  
+        public class StateObject
+        {
+            // Client  socket.  
+            public Socket workSocket = null;
+            // Size of receive buffer.  
+            public const int BufferSize = 1024;
+            // Receive buffer.  
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.  
+            public StringBuilder sb = new StringBuilder();
+        }
+
+
+        // Thread signal.  
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.  
+            // The DNS name of the computer  
+            // running the listener is "host.contoso.com".  
+            //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPAddress ipAddress = IPAddress.Any;
+
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8088);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.  
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.  
+                    Console.WriteLine("Waiting for a connection...");
+                    log.Info("等待TCP客户端连接...");
+
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback),
+                        listener);
+
+                    // Wait until a connection is made before continuing.  
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                log.Error("错误信息："+ e.Message);
+            }
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.  
+            allDone.Set();
+
+            // Get the socket that handles the client request.  
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Create the state object.  
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+        }
+
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket  
+            // from the asynchronous state object.  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            string workSocketIP =state.workSocket.RemoteEndPoint.ToString();
+            // Read data from the client socket.   
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+                state.sb.Clear();
+                // There  might be more data, so store the data received so far.  
+                state.sb.Append(Encoding.ASCII.GetString(
+                    state.buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read   
+                // more data.  
+                content = state.sb.ToString();
+                Console.WriteLine("接收到客户端：{0} 的数据为{1} bytes \n 内容 : {2}", workSocketIP, content.Length, content);
+                log.InfoFormat("接收到客户端：{0} 的数据为{1} bytes \n 内容 : {2}", workSocketIP, content.Length, content);
+
+                string SendStr = "Servers at ["+DateTime.Now.ToString("yyy-MM-dd HH:mm:ss")+"] Receive your Data:" +content;
+
+                Send(handler, SendStr);
+                log.InfoFormat("发送给客户端：{0} 的数据为{1} bytes \n 内容 : {2}", workSocketIP, SendStr.Length, SendStr);
+
+
+
+                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                new AsyncCallback(ReadCallback), state);
+               
+            }
+        }
+
+        private static void Send(Socket handler, String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+
+            // Begin sending the data to the remote device.  
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.  
+                Socket handler = (Socket)ar.AsyncState;
+
+                // Complete sending the data to the remote device.  
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //点击"是(YES)"退出程序  
+            if (MessageBox.Show("确定要退出程序?", "重要提示",
+                        System.Windows.Forms.MessageBoxButtons.YesNo,
+                        System.Windows.Forms.MessageBoxIcon.Warning)
+                == System.Windows.Forms.DialogResult.Yes)
+            {
+                notifyIcon1.Visible = false;   //设置图标不可见  
+                this.Close();                  //关闭窗体  
+                this.Dispose();                //释放资源  
+                Application.Exit();            //关闭应用程序窗体  
+            }
+        }
+    }
+}
